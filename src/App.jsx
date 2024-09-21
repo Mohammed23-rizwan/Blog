c8daee8cd5d3c88ae9f4ec8faed22377547c904c
Mohammed23-rizwan <rizuart23@gmail.com>
2024-09-22 02:27:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Bloghero from "./Components/BlogHero/Bloghero";
+
+const App = () => {
+  return (
+    <>
+      <Bloghero />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/card/card.jsx
@@ -0,0 +1,11 @@
+const card = () => {
+  return (
+    <div className="w-full grid grid-cols-3 gap-3 place-items-center	">
+      <div className="container w-80 h-96 bg-black"></div>
+      <div className="container w-80 h-96 bg-black"></div>
+      <div className="container w-80 h-96 bg-black"></div>
+    </div>
+  );
+};
+
+export default card;
